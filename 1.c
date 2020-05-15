fc1ad013f690df78962bd864fad53a4386761d43
RensLi <nightzijiang@outlook.com>
2020-05-15 16:58:21 +0800

    1.c change add_(char)str(void)

--- 1.c
+++ 1.c
@@ -3,10 +3,18 @@ void main()
 {
   int i = 2+2;
   int j = 2;
+  char s = str();
   printf("%d,%d",i.j);
+  pritnf("s is : %s",s);
 }
 
 int cc()
 {
   return 3;
+}
+
+char str()
+{
+  char st = "a";
+  return st;
 }
